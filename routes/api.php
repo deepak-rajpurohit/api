0f6ceb0089a3f5efc2ca9ccd5c4c457c8930500e
Deepak Rajpurohit <deepak.codoffer@gmail.com>
2024-11-08 15:13:02 +0530

    get api

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get("/test",function(){
+    return ["name"=>"Deepak", "email"=>"deepak@email.com", "phone"=>"998877"];
+});
+
+Route::get('list',[StudentController::class,'list']);
