d6bfc9e3cf5843fa833a282d0c2f021c2e7dfd85
Deepak Rajpurohit <deepak.codoffer@gmail.com>
2024-11-11 14:56:11 +0530

    SignUp and Login api

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
 {
     //
     function login(Request $request){
-        return "Login function";
+        // $input = $request->all();
+        // return $input;
+        $user = User::where('email', $request->email)->first();
+        if(!$user || ! Hash::check($request->password , $user->password)){
+            return ['result'=>"User not found", 'success'=>false];
+        }
+        $success['token']=$user->createToken('MyApp')->plainTextToken;
+        $success['name']=$user->name;
+
+        return ['result'=>$success];
     }
+
 
     function signUp(Request $request){
         $input = $request->all();
